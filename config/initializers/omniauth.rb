a25a05ccae44245ec84a010df174ad6a6f5facf0
Zhenying Wu <rbwabd@yahoo.com>
2012-02-27 19:06:31 +0000

    set up test FB app

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,7 +6,7 @@ else
 	#if ENV['USER'] == 'monica'
 		Rails.application.config.middleware.use OmniAuth::Builder do
 			#provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
-			provider :facebook, '245402168879797', '460cfddb3c8dfb0bf70bc29c3a6676f6', {:scope => 'publish_stream,offline_access,email'} # Find these values on the Facebook App page
+			provider :facebook, '	242516295838836', '	5eea597f7b47e15da107328196f8e047', {:scope => 'publish_stream,offline_access,email'} # Find these values on the Facebook App page
 		end
 	#end
 end
