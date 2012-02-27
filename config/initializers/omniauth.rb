3497db11954d76d95b929997f0007bd4e00d7cd7
rbwabd <rbwabd@yahoo.com>
2012-02-27 20:06:52 +0000

    added pics but not working

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,12 +1,12 @@
 if Rails.env == "production"
 	Rails.application.config.middleware.use OmniAuth::Builder do
-		provider :facebook, '245402168879797', '460cfddb3c8dfb0bf70bc29c3a6676f6',{:scope => 'publish_stream,offline_access,email', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}   # Find these values on the Facebook App page
+		provider :facebook, '245402168879797', '460cfddb3c8dfb0bf70bc29c3a6676f6',{:scope => 'publish_stream,offline_access,email', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}   
 	end
 else
 	#if ENV['USER'] == 'monica'
 		Rails.application.config.middleware.use OmniAuth::Builder do
 			#provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
-			provider :facebook, '	242516295838836', '	5eea597f7b47e15da107328196f8e047', {:scope => 'publish_stream,offline_access,email'} # Find these values on the Facebook App page
+			provider :facebook, '	242516295838836', '	5eea597f7b47e15da107328196f8e047', {:scope => 'publish_stream,offline_access,email'} 
 		end
 	#end
 end
