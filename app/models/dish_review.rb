b675a7320ecd7cb7a670c736a14912a2f1ce57e3
Zhenying Wu <rbwabd@yahoo.com>
2012-03-15 12:58:45 +0000

    added picture fields

--- app/models/dish_review.rb
+++ app/models/dish_review.rb
@@ -1,9 +1,11 @@
 class DishReview < ActiveRecord::Base
-  attr_accessible :user_id, :dish_id, :rating, :tagline, :review, :dish
+  attr_accessible :user_id, :dish_id, :rating, :tagline, :review, :dish, :pictures_attributes
   
   belongs_to :user
   belongs_to :visit
   belongs_to :dish
   
   has_many :pictures
+  accepts_nested_attributes_for :pictures
+    
 end
