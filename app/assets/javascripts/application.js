e332be1b280ee049bfbda8eb470ab5b80a1470c7
rbwabd <rbwabd@yahoo.com>
2012-03-25 20:14:36 +0100

    changed jquery style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,18 +4,18 @@
 //= require jquery-ui
 //= require_tree .
 
-jQuery.noConflict();
+var $ = jQuery.noConflict();
 
 //function to prevent submission of form on enter
-jQuery(function() {
-  jQuery("input").not( jQuery(":button") ).keypress(function (evt) {
+$(function() {
+  $("input").not( $(":button") ).keypress(function (evt) {
     if (evt.keyCode == 13) {
-      iname = jQuery(this).val();
+      iname = $(this).val();
       nestedFormEvents.addFields("add_nested_fields");
       
       // if Value of field is submit, submit the form. need to change this
       if (iname !== 'Submit'){  
-        var fields = jQuery(this).parents('form:eq(0),body').find('button,input,textarea,select');
+        var fields = $(this).parents('form:eq(0),body').find('button,input,textarea,select');
         var index = fields.index( this );
         if ( index > -1 && ( index + 1 ) < fields.length ) {
           fields.eq( index + 1 ).focus();
@@ -27,18 +27,18 @@ jQuery(function() {
 });
 
 // default tab option e.g. to show menu
-jQuery(function() {
-  jQuery("#tabs").tabs()
+$(function() {
+  $("#tabs").tabs()
 });
 
 // tab for sorting menu tab and item orders
-jQuery(function() {
-  var num_lists = jQuery('[id^=taborder]').length;
+$(function() {
+  var num_lists = $('[id^=taborder]').length;
   for(cnt=0;cnt<num_lists;cnt++){
-    jQuery( "#sortable"+cnt ).sortable({
+    $( "#sortable"+cnt ).sortable({
       update: function(event,ui) {
-        jQuery(this).find(".ui-state-default").each(function(i){
-          var val = jQuery(this).attr("id");
+        $(this).find(".ui-state-default").each(function(i){
+          var val = $(this).attr("id");
           var part = val.split("_");
           document.getElementById("dd_"+part[1]).value = i;
         });
@@ -46,12 +46,12 @@ jQuery(function() {
     }).disableSelection();
   }
     
-  var jQuerytabs = jQuery( "#tabssortable" ).tabs().find( ".ui-tabs-nav" ).sortable({ 
+  var $tabs = $( "#tabssortable" ).tabs().find( ".ui-tabs-nav" ).sortable({ 
                             axis: "x",
                             update: function(event, ui){
-                              //alert(jQuery(this).text());
-                              jQuery(this).find("a").each(function(i){
-                                var val = jQuery(this).attr("href");
+                              //alert($(this).text());
+                              $(this).find("a").each(function(i){
+                                var val = $(this).attr("href");
                                 var part = val.split("_");
                                 document.getElementById("taborder_"+part[1]).value = i;
                               });
@@ -60,33 +60,33 @@ jQuery(function() {
 });
 
 // datepicker in add visit form
-jQuery(function() {
-  jQuery( "#visit_visit_date" ).datepicker({ dateFormat: 'yy-mm-dd' });
+$(function() {
+  $( "#visit_visit_date" ).datepicker({ dateFormat: 'yy-mm-dd' });
 });
 
 // autocomplete used in search store and add dishes to visit
-jQuery(function() {
-  jQuery('#city_name').autocomplete({
+$(function() {
+  $('#city_name').autocomplete({
     minLength: 3,
     source: '/autocomplete/cities',
     focus: function(event, ui) {
-      jQuery('#city_name').val(ui.item.name);
+      $('#city_name').val(ui.item.name);
       return false;
     },
     select: function(event, ui) {
-      jQuery('#city_id').val(ui.item.id);
+      $('#city_id').val(ui.item.id);
       return false;
     }
   });
-  jQuery('#store_name').autocomplete({
+  $('#store_name').autocomplete({
     minLength: 3,
     source: function(request, response) {
-      jQuery.ajax({
+      $.ajax({
         url: "/autocomplete/stores",
         dataType: "json",
         data: {
           term : request.term,
-          city_id : jQuery('#city_id').val()
+          city_id : $('#city_id').val()
         },
         success: function(data) {
           response(data);
@@ -94,24 +94,24 @@ jQuery(function() {
       });
     },
     focus: function(event, ui) {
-      jQuery('#store_name').val(ui.item.name);
+      $('#store_name').val(ui.item.name);
       return false;
     },
     select: function(event, ui) {
-      jQuery('#store_id').val(ui.item.id);
-      jQuery("#display").load('stores/'+jQuery('#store_id').val()+" #store");
+      $('#store_id').val(ui.item.id);
+      $("#display").load('stores/'+$('#store_id').val()+" #store");
       return false;
     }
   });
-  jQuery('#visit_dish_name').autocomplete({
+  $('#visit_dish_name').autocomplete({
     minLength: 3,
     source: function(request, response) {
-      jQuery.ajax({
+      $.ajax({
         url: "/autocomplete/dishes",
         dataType: "json",
         data: {
           term : request.term,
-          store_id : jQuery('#store_id').val()
+          store_id : $('#store_id').val()
         },
         success: function(data) {
           response(data);
@@ -119,9 +119,9 @@ jQuery(function() {
       });
     },
     select: function(event, ui) {
-      jQuery('#visit_dish_id').val(ui.item.id);
-      jQuery('#visit_dish_name').val('');
-      jQuery("#cart").load('/change_cart', { 'dish_id': ui.item.id, 'dish_name': ui.item.name } );
+      $('#visit_dish_id').val(ui.item.id);
+      $('#visit_dish_name').val('');
+      $("#cart").load('/change_cart', { 'dish_id': ui.item.id, 'dish_name': ui.item.name } );
       return false;
     }
   });
