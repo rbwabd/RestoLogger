6e0ecccd555ac5fe408997a0cf31c7e93e53d630
rbwabd <rbwabd@yahoo.com>
2012-03-26 09:59:28 +0100

    dff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,13 +9,13 @@ RestoLogger::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -58,7 +58,7 @@ RestoLogger::Application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
-
+  config.assets.debug = true
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
